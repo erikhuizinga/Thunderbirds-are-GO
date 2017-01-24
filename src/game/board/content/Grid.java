617895c78aa2fffaecdc1a56bfa28e381066f109
Erik Huizinga <erik.huizinga@nedap.com>
2017-01-24 13:34:25 +0100

    Stub abstract Grid

--- src/game/board/content/Grid.java
+++ src/game/board/content/Grid.java
@@ -19,10 +19,20 @@ public abstract class Grid {
    * y} vertically incremental from the top.
    */
   private final Map<int[], Integer> sub2IndMap = new HashMap<>();
-  private final Map<Integer, int[]> ind2SubMap = new HashMap<>();
 
-  public Grid(int boardSideLength) {
-    init(boardSideLength);
+  /**
+   * The map of linear indices to subscript indices.
+   */
+  private final Map<Integer, int[]> ind2SubMap = new HashMap<>();
+  private final int dim;
+
+  public Grid(int dim) {
+    init();
+    this.dim = dim;
+  }
+
+  public int getDim() {
+    return dim;
   }
 
   /**
@@ -35,13 +45,12 @@ public abstract class Grid {
   /**
    * Initialise the {@code Grid} by setting all values to {@code Point.EMPTY} and initialise
    * the {@code sub2Ind} and {@code ind2Sub} methods.
-   *
-   * @param dim The length of the {@code game.board.Board}}.
    */
-  public void init(int dim) {
+  protected void init() {
+    int dim = getDim();
     int sub[] = new int[2];
     for (int ind = 0; ind < dim; ind++) {
-      grid.put(ind, new Point(Point.EMPTY));
+      getGrid().put(ind, new Point(Point.EMPTY));
       sub[0] = ind / dim;  // Get column, i.e., x
       sub[1] = ind % dim;  // Get row, i.e., y
       ind2SubMap.put(ind, sub);
