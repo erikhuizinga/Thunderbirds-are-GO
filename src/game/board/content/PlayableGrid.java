2bff84e032931f5aa021b6a840bd01bf37e97564
Erik Huizinga <erik.huizinga@nedap.com>
2017-01-24 12:24:45 +0100

    Stub PlayableGrid

---
+++ src/game/board/content/PlayableGrid.java
@@ -0,0 +1,11 @@
+package game.board.content;
+
+/**
+ * Created by erik.huizinga on 24-1-17.
+ */
+public class PlayableGrid extends Grid {
+
+  public PlayableGrid(int boardSideLength) {
+    super(boardSideLength);
+  }
+}
