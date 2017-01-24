ae293b63e6eaa1e57247e8763f9c89c88a657c52
Erik Huizinga <erik.huizinga@nedap.com>
2017-01-24 14:11:40 +0100

    Stub GridTest

---
+++ src/test/game/board/content/GridTest.java
@@ -0,0 +1,36 @@
+package test.game.board.content;
+
+import game.board.content.Grid;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by erik.huizinga on 24-1-17.
+ */
+class GridTest {
+
+  Grid grid1;
+  Grid grid2;
+
+  @BeforeEach
+  void setUp() {
+    grid1 = new Grid(1);
+    grid2 = new Grid(2);
+  }
+
+  @Test
+  void init() {
+
+  }
+
+  @Test
+  void sub2Ind() {
+
+  }
+
+  @Test
+  void ind2Sub() {
+
+  }
+
+}
