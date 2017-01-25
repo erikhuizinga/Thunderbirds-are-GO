07291f19996ebe308b075d1c2725724b82310323
Erik Huizinga <erik.huizinga@nedap.com>
2017-01-25 10:19:19 +0100

    Add Board Javadoc

--- src/game/board/Board.java
+++ src/game/board/Board.java
@@ -4,18 +4,34 @@ import game.board.content.Grid;
 import java.util.Observable;
 
 /**
+ * A Go board.
  * Created by erik.huizinga on 23-1-17.
  */
 public class Board extends Observable {
 
+  /**
+   * The single-side dimension of the playable square grid.
+   */
   private final int dim;
+
+  /**
+   * The grid in which the board's content is stored.
+   */
   private final Grid grid;
 
+  /**
+   * Construct a Go board with the specified single-side dimension of the playable grid.
+   *
+   * @param dim the dimension.
+   */
   Board(int dim) {
     this.dim = dim;
     grid = new Grid(dim);
   }
 
+  /**
+   * @return the single-side dimension of the playable square grid.
+   */
   public int getDim() {
     return dim;
   }
