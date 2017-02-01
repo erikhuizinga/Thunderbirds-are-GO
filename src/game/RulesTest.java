d8eb9054d7868dd641d63c64198adb4701bd1e57
Erik Huizinga <erik.huizinga@nedap.com>
2017-02-01 10:37:15 +0100

    Update unit tests of rules

--- src/game/RulesTest.java
+++ src/game/RulesTest.java
@@ -9,11 +9,7 @@ import game.action.Move;
 import game.material.Stone;
 import game.material.board.Board;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileReader;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.Writer;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -41,17 +37,32 @@ class RulesTest {
           Arrays.asList(3, 5),
           Arrays.asList(5, 2),
           Arrays.asList(4, 1),
-          Arrays.asList(5, 0));
+          Arrays.asList(3, 0),
+          Arrays.asList(5, 0),
+          Arrays.asList(2, 1),
+          Arrays.asList(2, 2));
   private List<List<Integer>> blackIndices2 =
       Arrays.asList(
           Arrays.asList(2, 2),
           Arrays.asList(1, 1),
           Arrays.asList(0, 2),
-          Arrays.asList(1, 4),
+          Arrays.asList(1, 4), // Suicide of one stone: normally not allowed due to historical
+          // validity
           Arrays.asList(3, 4),
           Arrays.asList(3, 3),
           Arrays.asList(3, 5),
           Arrays.asList(1, 4));
+  private List<Move> blackKoMoves =
+      Arrays.asList(
+          new Move(5, 0, Stone.BLACK), // Suicide of one stone: normally not
+          // allowed due to historical validity
+          new Move(5, 2, Stone.BLACK),
+          new Move(4, 1, Stone.BLACK),
+          new Move(5, 0, Stone.BLACK));
+  private Move whiteIllegalKoMove = new Move(5, 1, Stone.WHITE);
+  private List<Move> legalMovesAfterKo =
+      Arrays.asList(
+          new Move(1, 0, Stone.WHITE), new Move(0, 1, Stone.BLACK), new Move(5, 1, Stone.WHITE));
 
   @BeforeEach
   void setUp() {
@@ -100,7 +111,7 @@ class RulesTest {
       System.out.println(board);
 
       boardPath = "src/game/RulesTestOutput/board1_" + i + ".txt";
-//      /*
+      /*
       // Write some boards to text files
       try {
         Writer writer = new PrintWriter(new FileOutputStream(boardPath));
@@ -114,7 +125,7 @@ class RulesTest {
         e.printStackTrace();
         System.exit(2);
       }
-//      */
+            */
 
       // Read reference board
       String boardString = "";
@@ -153,7 +164,7 @@ class RulesTest {
       System.out.println(board);
 
       boardPath = "src/game/RulesTestOutput/board2_" + i + ".txt";
-      // /*
+      /*
       // Write some boards to text files
       try {
         Writer writer = new PrintWriter(new FileOutputStream(boardPath));
@@ -167,7 +178,7 @@ class RulesTest {
         e.printStackTrace();
         System.exit(2);
       }
-      // */
+       */
 
       // Read reference board
       String boardString = "";
@@ -193,13 +204,41 @@ class RulesTest {
 
   @Test
   void testIsHistoricallyValid() {
-    // Test that the initial board is not in the history
+    // Test that the initial board is in the history
     assertFalse(Rules.isHistoricallyValid(go, go.getBoard()));
     // Test that another board is not in the history
     assertTrue(Rules.isHistoricallyValid(go, board));
     // Test that adding another board to history will make it historically invalid
     go.addHistoryRecord(board);
     assertFalse(Rules.isHistoricallyValid(go, board));
+
+    // Ko
+    go = new Go(dim, null, null);
+    for (Move blackKoMove : blackKoMoves) {
+      board = blackKoMove.apply(board);
+      Rules.handleDynamicalValidity(board, blackKoMove);
+      assertTrue(Rules.isHistoricallyValid(go, board));
+      go.addHistoryRecord(board);
+      System.out.println("After playing a black move towards Ko and ensuring validity:");
+      System.out.println(board);
+    }
+
+    // Play an illegal move and assert that it is historically invalid
+    Board illegalBoard = whiteIllegalKoMove.apply(board);
+    Rules.handleDynamicalValidity(illegalBoard, whiteIllegalKoMove);
+    assertFalse(Rules.isHistoricallyValid(go, illegalBoard));
+    System.out.println("Illegal Ko move applied:");
+    System.out.println(illegalBoard);
+
+    // Play legal different moves and assert that this is historically valid
+    for (Move legalMoveAfterKo : legalMovesAfterKo) {
+      board = legalMoveAfterKo.apply(board);
+      Rules.handleDynamicalValidity(board, legalMoveAfterKo);
+      assertTrue(Rules.isHistoricallyValid(go, board));
+      go.addHistoryRecord(board);
+      System.out.println("After playing moves after Ko and ensuring validity:");
+      System.out.println(board);
+    }
   }
 
   @Test
